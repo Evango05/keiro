add2142de0dcb5d61a0627da905149fbb40cfae2
Evan Fabiani <fabiani.evan@icloud.com>
2023-03-07 16:30:28 +0100

    geocoder et mapbox completed !

--- app/models/hotspot.rb
+++ app/models/hotspot.rb
@@ -4,4 +4,7 @@ class Hotspot < ApplicationRecord
 
   has_many :categories, through: :hotcats
   has_many :itineraries, through: :hotitis
+
+  geocoded_by :address
+  after_validation :geocode, if: :will_save_change_to_address?
 end
