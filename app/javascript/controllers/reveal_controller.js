c365267458edb2f7bb16cb71d3fc113f78f6ae2d
Adrien Lupo <adrien.lupo@protonmail.com>
2023-03-13 13:12:57 +0100

    grey filter

--- app/javascript/controllers/reveal_controller.js
+++ app/javascript/controllers/reveal_controller.js
@@ -9,9 +9,8 @@ export default class extends Reveal {
   }
 
   show(event) {
-    // console.log(event.params["index"])
-    // console.log(this.photoTargets)
     const div = this.photoTargets[event.params["index"]]
     this.itemTarget.innerHTML = `${div.getAttribute("data-value")} <br> <br> ${div.getAttribute("data-value-2")}`
+    console.log(div.classList.toggle("item-grey"))
   }
 }
