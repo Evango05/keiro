b249e48831f219ff492c96fcf47e804d7e634170
Adrien Lupo <adrien.lupo@protonmail.com>
2023-03-16 18:44:36 +0100

    progress map

--- app/javascript/controllers/recap_controller.js
+++ app/javascript/controllers/recap_controller.js
@@ -2,20 +2,19 @@ import { Controller } from "@hotwired/stimulus"
 
 // Connects to data-controller="recap"
 export default class extends Controller {
-  static targets = ["button_fav", "button_share"]
+  static targets = ["button_fav", "button_share", "star"]
 
 
   connect() {
-
-    console.log("hello from recap")
   }
 
   favorite(){
-    this.button_favTarget.classList.remove("d-none")
+    this.button_favTarget.classList.toggle("d-none")
+    this.starTarget.classList.toggle("yellow_star")
 
   }
 
   share(){
-    this.button_shareTarget.classList.remove("d-none")
+    this.button_shareTarget.classList.toggle("d-none");
   }
 }
